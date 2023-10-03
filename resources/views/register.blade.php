024dcaaa857e6b6234b2b0579f9f674d485f3c63
Harrif118 <arifsaputrapku2006@gmail.com>
2023-10-03 11:38:33 +0700

    Tugas Laravel

---
+++ resources/views/register.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Register</title>
+</head>
+<body>
+    <h1>Register</h1>
+    <form method="POST" action="/welcome">
+        @csrf
+        <label for="first_name">First Name:</label>
+        <input type="text" name="first_name" required><br>
+
+        <label for="last_name">Last Name:</label>
+        <input type="text" name="last_name" required><br>
+
+        <button type="submit">Submit</button>
+    </form>
+</body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Welcome</title>
+</head>
+<body>
+    <h1>Welcome to Our Website, {{ $firstName }} {{ $lastName }}!</h1>
+    <a href="/">Back to Home</a>
+</body>
+</html>
